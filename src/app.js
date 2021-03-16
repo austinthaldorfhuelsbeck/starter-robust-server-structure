03df627cae2f8d1f178163c7479381e171b7275e
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-03-16 14:17:27 -0700

    handlers for /flips + /flips/:flipId paths

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
+const flips = require("./data/flips-data"); // reads, executes, returns `exports` object
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+app.use("/flips/:flipId", (req, res, next) => {
+  // define a handler for the path
+  const { flipId } = req.params; // destructure flipId variable from params
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId)); // find() flip by id
+
+  if (foundFlip) {
+    res.json({ data: foundFlip }); // send data containing foundFlip object to client as JSON
+  } else {
+    next(`Flip id not found: ${flipId}`); // next() with err message if not found
+  }
+});
+
+app.use("/flips", (req, res) => {
+  // define a handler for the path
+  res.json({ data: flips }); // method to tell express to respond with JSON
+});
 
 // Not found handler
 app.use((request, response, next) => {
