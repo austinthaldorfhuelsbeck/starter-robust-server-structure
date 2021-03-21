75110ddeb8174766339455cc44c27604126d6dd1
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-03-20 17:25:51 -0700

    validation middleware function

--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@ const app = express();
 const flips = require("./data/flips-data"); // reads, executes, returns `exports` object
 const counts = require("./data/counts-data");
 
+function bodyHasResultProperty(req, res, next) {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    return next();
+  }
+  next("A 'result' property is required.");
+}
+
 app.use(express.json());
 
 app.use("/counts/:countId", (req, res, next) => {
@@ -37,16 +45,20 @@ app.get("/flips", (req, res) => {
   res.json({ data: flips }); // method to tell express to respond with JSON
 });
 
-app.post("/flips", (req, res, next) => {
-  const { data: { result } = {} } = req.body;
-  const newFlip = {
-    id: flips.length + 1, // Assign the next ID
-    result,
-  };
-  flips.push(newFlip);
-  counts[result] = counts[result] + 1; // Increment the counts
-  res.status(201).json({ data: newFlip });
-});
+app.post(
+  "/flips",
+  bodyHasResultProperty, // Add validation middleware function
+  (req, res) => {
+    // Route handler no longer has validation code.
+    const { data: { result } = {} } = req.body;
+    const newFlip = {
+      id: flips.length + 1, // Assign the next ID
+      result: result,
+    };
+    flips.push(newFlip);
+    res.status(201).json({ data: newFlip });
+  }
+);
 
 // Not found handler
 app.use((request, response, next) => {
